bd3091aa023c24b02939a9845337251ce1d66569
Jhonatan Steven Rodriguez Ramirez <dev.jhonatanramirez@gmail.com>
2024-07-22 14:21:40 -0500

    first commit

--- Latest/main.py
+++ Latest/main.py
@@ -9,7 +9,6 @@ load_dotenv()
 # Acceder a las variables de entorno
 api_key = os.getenv('API_KEY')
 model_name = os.getenv('MODEL')
-db_pass = os.getenv('DB_PASS')
 
 chatbot = AgentHandler(api_key,tools,"claude-3-haiku-20240307")
 chatbot.display_graph()
